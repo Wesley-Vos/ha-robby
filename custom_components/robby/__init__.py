317d277b7f78e80803e7b764f92625f3d038b5cb
Wesley Vos <wesley.vos@kpn.com>
2025-04-14 15:50:10 +0200

    Add datetime entities for cycles

--- custom_components/robby/__init__.py
+++ custom_components/robby/__init__.py
@@ -2,34 +2,184 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass
-
+from homeassistant.components.lawn_mower import LawnMowerActivity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    STATE_UNAVAILABLE,
+    STATE_UNKNOWN,
+    Platform,
+)
+from homeassistant.core import Event, EventStateChangedData, HomeAssistant, ServiceCall
+from homeassistant.helpers.event import async_track_state_change_event
+from homeassistant.util.dt import now
+
+from .const import STATE_CHARGING
 
 _PLATFORMS: list[Platform] = [
     Platform.BINARY_SENSOR,
+    Platform.DATETIME,
     Platform.LAWN_MOWER,
     Platform.SWITCH,
 ]
 
-type RobbyConfigEntry = ConfigEntry[RobbyData]  # noqa: F821
+_ROBBY_START_CHARGING_CYCLE_ENTITY_ID = "datetime.robby_start_charging_cycle"
+_ROBBY_END_CHARGING_CYCLE_ENTITY_ID = "datetime.robby_end_charging_cycle"
+_ROBBY_START_MOWING_CYCLE_ENTITY_ID = "datetime.robby_start_mowing_cycle"
+_ROBBY_END_MOWING_CYCLE_ENTITY_ID = "datetime.robby_end_mowing_cycle"
+_ROBBY_SWITCH_STUCK_ENTITY_ID = "switch.robby_stuck"
 
-
-@dataclass
-class RobbyData:
-    """Data for Robby."""
-
-    is_stuck: bool
+type RobbyConfigEntry = ConfigEntry  # noqa: F821
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: RobbyConfigEntry) -> bool:
     """Set up Robby from a config entry."""
 
-    entry.runtime_data = RobbyData(is_stuck=False)
-
     await hass.config_entries.async_forward_entry_setups(entry, _PLATFORMS)
+
+    """Actions"""
+
+    # def is_mowing() -> bool:
+    #     """Check if the Robby is mowing according to dts."""
+    #     start_dt = parse_datetime(
+    #         hass.states.get(_ROBBY_START_MOWING_CYCLE_ENTITY_ID).state
+    #     )
+    #     stop_dt = parse_datetime(
+    #         hass.states.get(_ROBBY_STOP_MOWING_CYCLE_ENTITY_ID).state
+    #     )
+
+    #     return start_dt > stop_dt
+
+    # def is_charging() -> bool:
+    #     """Check if the Robby is charging according to dts."""
+    #     start_dt = parse_datetime(
+    #         hass.states.get(_ROBBY_START_CHARGING_CYCLE_ENTITY_ID).state
+    #     )
+    #     stop_dt = parse_datetime(
+    #         hass.states.get(_ROBBY_STOP_CHARGING_CYCLE_ENTITY_ID).state
+    #     )
+
+    #     return start_dt > stop_dt
+
+    async def start_charging() -> None:
+        """Start charging the Robby."""
+        await hass.services.async_call(
+            "datetime",
+            "set_value",
+            {
+                ATTR_ENTITY_ID: _ROBBY_START_CHARGING_CYCLE_ENTITY_ID,
+                "datetime": now().isoformat(),
+            },
+            blocking=False,
+        )
+
+    async def stop_charging() -> None:
+        """Stop charging the Robby."""
+        await hass.services.async_call(
+            "datetime",
+            "set_value",
+            {
+                ATTR_ENTITY_ID: _ROBBY_END_CHARGING_CYCLE_ENTITY_ID,
+                "datetime": now().isoformat(),
+            },
+            blocking=False,
+        )
+
+    async def start_mowing() -> None:
+        """Start mowing with the Robby."""
+        await hass.services.async_call(
+            "datetime",
+            "set_value",
+            {
+                ATTR_ENTITY_ID: _ROBBY_START_MOWING_CYCLE_ENTITY_ID,
+                "datetime": now().isoformat(),
+            },
+            blocking=False,
+        )
+
+    async def stop_mowing() -> None:
+        """Stop mowing with the Robby."""
+        await hass.services.async_call(
+            "datetime",
+            "set_value",
+            {
+                ATTR_ENTITY_ID: _ROBBY_END_MOWING_CYCLE_ENTITY_ID,
+                "datetime": now().isoformat(),
+            },
+            blocking=False,
+        )
+
+    async def getting_stuck() -> None:
+        """Handle the Robby getting stuck."""
+        await hass.services.async_call(
+            "homeassistant",
+            "turn_on",
+            {"entity_id": _ROBBY_SWITCH_STUCK_ENTITY_ID},
+            blocking=False,
+        )
+
+    async def released() -> None:
+        """Handle the Robby being released."""
+        await hass.services.async_call(
+            "homeassistant",
+            "turn_off",
+            {"entity_id": _ROBBY_SWITCH_STUCK_ENTITY_ID},
+            blocking=False,
+        )
+
+    async def async_handle_state_change(
+        event: Event[EventStateChangedData],
+    ) -> None:
+        """Process state change event."""
+        data = event.data
+        old_state = data.get("old_state").state
+        new_state = data.get("new_state").state
+
+        print("Transition from", old_state, "to", new_state)
+
+        if old_state == new_state:
+            return
+
+        if old_state in (STATE_UNAVAILABLE, STATE_UNKNOWN) or new_state in (
+            STATE_UNAVAILABLE,
+            STATE_UNKNOWN,
+        ):
+            # Handle unavailable or unknown state
+            return
+
+        # Handle other state changes
+        if old_state == LawnMowerActivity.DOCKED:
+            if new_state == LawnMowerActivity.MOWING:
+                print("Started mowing after being dokced")
+                await start_mowing()
+            elif new_state == STATE_CHARGING:
+                print("Started charging")
+                await start_charging()
+        elif old_state == STATE_CHARGING:
+            await stop_charging()
+            if new_state == LawnMowerActivity.MOWING:
+                await start_mowing()
+                print("Finished charging and started mowing")
+            if new_state == LawnMowerActivity.DOCKED:
+                print("Finished charging")
+        elif old_state in (LawnMowerActivity.MOWING, LawnMowerActivity.ERROR):
+            if new_state == LawnMowerActivity.DOCKED:
+                print("Finished mowing and returned to dock without charging")
+                await stop_mowing()
+                await released()
+            elif new_state == STATE_CHARGING:
+                print("Finished mowing, returned to dock and started charging")
+                await stop_mowing()
+                await released()
+                await start_charging()
+
+    entry.async_on_unload(
+        async_track_state_change_event(
+            hass,
+            "lawn_mower.robby",
+            async_handle_state_change,
+        )
+    )
 
     return True
 
---
+++ custom_components/robby/datetime.py
@@ -0,0 +1,88 @@
+"""Robby Lawn Mower Entity for Home Assistant."""
+
+from datetime import datetime
+
+from homeassistant.components.datetime import DateTimeEntity, DateTimeEntityDescription
+from homeassistant.const import STATE_UNAVAILABLE, STATE_UNKNOWN
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+from homeassistant.helpers.restore_state import RestoreEntity
+
+from . import RobbyConfigEntry
+from .const import CONF_POWER_SENSOR, CONF_SWITCH_SENSOR
+
+ENTITIES: tuple[DateTimeEntityDescription, ...] = (
+    DateTimeEntityDescription(
+        key="robby_start_mowing_cycle",
+        name="Robby start mowing cycle",
+        icon="mdi:clock-start",
+    ),
+    DateTimeEntityDescription(
+        key="robby_end_mowing_cycle",
+        name="Robby end mowing cycle",
+        icon="mdi:clock-end",
+    ),
+    DateTimeEntityDescription(
+        key="robby_start_charging_cycle",
+        name="Robby start charging cycle",
+        icon="mdi:clock-start",
+    ),
+    DateTimeEntityDescription(
+        key="robby_end_charging_cycle",
+        name="Robby end charging cycle",
+        icon="mdi:clock-end",
+    ),
+)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: RobbyConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    """Set up the datetime entities."""
+    entities: list = [
+        RobbyDateTimeEntity(hass, entry, description) for description in ENTITIES
+    ]
+    async_add_entities(entities, update_before_add=True)
+
+
+class RobbyDateTimeEntity(DateTimeEntity, RestoreEntity):
+    """Representation of a Robby datetime entity."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        entry: RobbyConfigEntry,
+        description: DateTimeEntityDescription,
+    ) -> None:
+        """Initialize the datetime entity."""
+        self.hass = hass
+        self.entry = entry
+        self._power_sensor = entry.data[CONF_POWER_SENSOR]
+        self._switch_entity = entry.data[CONF_SWITCH_SENSOR]
+        self._attr_name = description.name
+        self._attr_unique_id = (
+            f"{description.key}_{self._power_sensor}_{self._switch_entity}"
+        )
+        self._state: datetime | None = None
+
+    @property
+    def native_value(self):
+        """Return the state of the datetime."""
+        return self._state
+
+    async def async_added_to_hass(self) -> None:
+        """Run when entity about to be added."""
+        await super().async_added_to_hass()
+
+        state = await self.async_get_last_state()
+        if not state or state.state in (STATE_UNKNOWN, STATE_UNAVAILABLE):
+            self._state = None
+        else:
+            self._state = datetime.fromisoformat(state.state)
+
+    async def async_set_value(self, value: datetime) -> None:
+        """Update the current value."""
+        self._state = value
+        self.async_write_ha_state()
